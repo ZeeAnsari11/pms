d785b903117452cb266146ff5c031c60610066d4
Mac-8 <mac-8@Mac-8s-MacBook-Pro.local>
2023-03-10 18:13:54 +0500

    PMA-591- Dashboard Design
    - Designed whole Dashboard
    
    - Made Navigation bars with buttons
    
    - Made sidebar with projects names
    
    - Made cards and boards with label chips and other information
    
     -Added functionality to make dynamic cards and boards
    
     - add functionality to delete a board and card
    
     - Added drag n drop functionality in the cards.
    
     - Made modal for cards
    
     - Design modal for cards and display different data on it.
    
     - Create different fields in modals to modify cards
    
     - Made scrollbars
    
     - made cards and boards

---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <h2>Projects</h2>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
