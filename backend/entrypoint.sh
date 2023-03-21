1485f1f202da8dfec8761e5ed0e69079396446c1
Zeeshan Bashi <zeeshan.bscs231@gmail.com>
2023-03-21 18:34:09 +0500

    update code

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
+if [ "$DATABASE" = "mysql" ]
 then
-    echo "Waiting for postgres..."
+    echo "Waiting for MYSQL Response..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $MYSQL_DATABASE_HOST $MYSQL_DATABASE_PORT; do
       sleep 0.1
     done
 
-    echo "Postgres started"
+    echo "MYSQL started"
 fi
 
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
-fi
 
 # python manage.py flush --no-input
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+echo "from django.contrib.auth.models import User;
+User.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
+User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 
 exec "$@"
