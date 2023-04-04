764542d3daedbf961ccbbca644375ed9f4efd765
Muhammad Asher <asher.khan@phpstudios.com>
2023-04-04 12:34:20 +0500

    Sidebar and Top Navbar with dropdown and searchBar

--- frontend/src/Components/Dashboard/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard/Dashboard.js
@@ -1,204 +1,192 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import "../../../App.css";
 import Board from '../Board/Board';
 import Editable from '../Editable/Editable';
-import Sidebar from '../Sidebar/Sidebar';
-import Dropdown from "../Dropdown/Dropdown";
-function Dashboard(props) {
-  const [showDropDown, setShowDropdown] = useState(false);
-  const [target, setTarget] = useState({
-    cid: "",
-    bid: "",
-  })
-  const [boards, setboards] = useState([
-    {
-      id: Date.now() + Math.random() * 2,
-      title: "Dashboard Frontend",
-      cards: [
-        {
-          id: Date.now() + Math.random(),
-          title: "Card 1",
-          tasks: [],
-          labels: [
-            {
-              text: "critical",
-              color: "red",
-            },
-          ],
-          desc: "Frontend of Dashboard",
-          date: "",
-        },
-        {
-          id: Date.now() + Math.random(),
-          title: "Card 2",
-          tasks: [],
-          labels: [
-            {
-              text: "In queue",
-              color: "Grey",
-            },
-          ],
-          desc: "Second frontend of Dashboard",
-          date: "",
-        },
-        {
-          id: Date.now() + Math.random(),
-          title: "Card 3",
-          tasks: [],
-          labels: [
-            {
-              text: "In Progress",
-              color: "Blue",
-            },
-          ],
-          desc: "Backend of Dashboard",
-          date: "",
-        },
-      ],
-    },
-  ]);
+import Sidebar from '../Sidebar/index';
+import NavBar from "../Navbar/index";
 
-  const addCard = (title, bid) => {
-    const card = {
-      id: Date.now() + Math.random(),
-      title,
-      labels: [],
-      tasks: [],
-      date: "",
-      desc: "",
+function Dashboard(props) {
+    const [target, setTarget] = useState({
+        cid: "",
+        bid: "",
+    })
+    const [boards, setboards] = useState([
+        {
+            id: Date.now() + Math.random() * 2,
+            title: "Dashboard Frontend",
+            cards: [
+                {
+                    id: Date.now() + Math.random(),
+                    title: "Card 1",
+                    tasks: [],
+                    labels: [
+                        {
+                            text: "critical",
+                            color: "red",
+                        },
+                    ],
+                    desc: "Frontend of Dashboard",
+                    date: "",
+                },
+                {
+                    id: Date.now() + Math.random(),
+                    title: "Card 2",
+                    tasks: [],
+                    labels: [
+                        {
+                            text: "In queue",
+                            color: "Grey",
+                        },
+                    ],
+                    desc: "Second frontend of Dashboard",
+                    date: "",
+                },
+                {
+                    id: Date.now() + Math.random(),
+                    title: "Card 3",
+                    tasks: [],
+                    labels: [
+                        {
+                            text: "In Progress",
+                            color: "Blue",
+                        },
+                    ],
+                    desc: "Backend of Dashboard",
+                    date: "",
+                },
+            ],
+        },
+    ]);
+
+    const addCard = (title, bid) => {
+        const card = {
+            id: Date.now() + Math.random(),
+            title,
+            labels: [],
+            tasks: [],
+            date: "",
+            desc: "",
+        };
+
+        const index = boards.findIndex((item) => item.id === bid)
+        if (index < 0) return;
+
+        const tempBoards = [...boards]
+        tempBoards[index].cards.push(card);
+        setboards(tempBoards);
     };
 
-    const index = boards.findIndex((item) => item.id === bid)
-    if (index < 0) return;
+    const removeCard = (cid, bid) => {
+        const bIndex = boards.findIndex((item) => item.id === bid);
+        if (bIndex < 0) return;
 
-    const tempBoards = [...boards]
-    tempBoards[index].cards.push(card);
-    setboards(tempBoards);
-  };
+        const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
+        if (cIndex < 0) return;
 
-  const removeCard = (cid, bid) => {
-    const bIndex = boards.findIndex((item) => item.id === bid);
-    if (bIndex < 0) return;
+        const tempBoards = [...boards];
+        tempBoards[bIndex].cards.splice(cIndex, 1);
+        setboards(tempBoards);
+    };
 
-    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
-    if (cIndex < 0) return;
+    const addBoard = (title) => {
+        setboards([
+            ...boards,
+            {
+                id: Date.now() + Math.random(),
+                title,
+                cards: [],
+            },
+        ]);
+    };
 
-    const tempBoards = [...boards];
-    tempBoards[bIndex].cards.splice(cIndex, 1);
-    setboards(tempBoards);
-  };
+    const removeBoard = bid => {
+        const tempBoards = boards.filter((item) => item.id !== bid);
 
-  const addBoard = (title) => {
-    setboards([
-      ...boards,
-      {
-        id: Date.now() + Math.random(),
-        title,
-        cards: [],
-      },
-    ]);
-  };
+        setboards(tempBoards);
+    };
 
-  const removeBoard = bid => {
-    const tempBoards = boards.filter((item) => item.id !== bid);
+    const handleDragEnter = (cid, bid) => {
+        setTarget({
+            cid,
+            bid,
+        });
+    };
 
-    setboards(tempBoards);
-  };
+    const handleDragEnd = (cid, bid) => {
+        let s_bIndex, s_cIndex, t_bIndex, t_cIndex;
 
-  const handleDragEnter = (cid, bid) => {
-    setTarget({
-      cid,
-      bid,
-    });
-  };
+        s_bIndex = boards.findIndex((item) => item.id === bid);
+        if (s_bIndex < 0) return;
 
-  const handleDragEnd = (cid, bid) => {
-    let s_bIndex, s_cIndex, t_bIndex, t_cIndex;
+        s_cIndex = boards[s_bIndex].cards?.findIndex((item) => item.id === cid);
+        if (s_cIndex < 0) return;
 
-    s_bIndex = boards.findIndex((item) => item.id === bid);
-    if (s_bIndex < 0) return;
+        t_bIndex = boards.findIndex((item) => item.id === target.bid);
+        if (t_bIndex < 0) return;
 
-    s_cIndex = boards[s_bIndex].cards?.findIndex((item) => item.id === cid);
-    if (s_cIndex < 0) return;
+        t_cIndex = boards[t_bIndex].cards?.findIndex((item) => item.id === target.cid);
+        if (t_cIndex < 0) return;
 
-    t_bIndex = boards.findIndex((item) => item.id === target.bid);
-    if (t_bIndex < 0) return;
+        const tempboards = [...boards];
+        const tempCard = tempboards[s_bIndex].cards[s_cIndex];
 
-    t_cIndex = boards[t_bIndex].cards?.findIndex((item) => item.id === target.cid);
-    if (t_cIndex < 0) return;
+        tempboards[s_bIndex].cards.splice(s_cIndex, 1);
 
-    const tempboards = [...boards];
-    const tempCard = tempboards[s_bIndex].cards[s_cIndex];
+        if (tempboards[t_bIndex].cards.length === 0) {
+            addCard(tempCard.title, tempboards[t_bIndex].id);
+        } else {
+            tempboards[t_bIndex].cards.splice(t_cIndex, 0, tempCard);
 
-    tempboards[s_bIndex].cards.splice(s_cIndex, 1);
-
-    if (tempboards[t_bIndex].cards.length === 0) {
-      addCard(tempCard.title, tempboards[t_bIndex].id);
-    } else {
-      tempboards[t_bIndex].cards.splice(t_cIndex, 0, tempCard);
-
-      setboards(tempboards);
+            setboards(tempboards);
+        }
     }
-  }
 
-  const updateCard = (bid, cid, card) => {
-      const bIndex = boards.findIndex((item) => item.id === bid);
-    if (bIndex < 0) return;
+    const updateCard = (bid, cid, card) => {
+        const bIndex = boards.findIndex((item) => item.id === bid);
+        if (bIndex < 0) return;
 
-    const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
-    if (cIndex < 0) return;
+        const cIndex = boards[bIndex].cards.findIndex((item) => item.id === cid);
+        if (cIndex < 0) return;
 
-      const tempBoards = [...boards];
-      tempBoards[bIndex].cards[cIndex] = card;
-      setboards(tempBoards);
+        const tempBoards = [...boards];
+        tempBoards[bIndex].cards[cIndex] = card;
+        setboards(tempBoards);
 
+    }
+
+    const project = {
+        name: "New Project",
+        category: 'SOFTWARE'
     }
 
     return (
 
         <div className="app">
-          <Sidebar/>
-          <div className="app_navbar_one">
-            <img src="https://lh3.google.com/u/0/d/1IxWCTZL5L9PRa1DwoKw8wu8EA7Ez_hT0=w2560-h848-iv1" alt="PHPStudios" />
-            <button id="btn_1" className='Dashboard_nav'>Only My Issue </button>
-            <button id="btn_2" className='Dashboard_nav'>Recently Updated</button>
-            <div className="user-profile">
-              <div className="user-profile-icon" onClick={() => setShowDropdown(!showDropDown)}>
-                <img src="user-image.jpg" alt="User Profile"/>
-                {showDropDown && (
-                  <Dropdown onClick={() => setShowDropdown(!showDropDown)}>
-                    <div className="board_dropdown">
-                      <p onClick={() => props.removeBoard(props.board?.id)}>Delete Board</p>
+            <Sidebar project={project}/>
+            <NavBar/>
+
+            <div className="app_outer">
+                <div className="app_boards">
+                    {
+                        boards.map((item) => (<Board
+                                key={item.id} board={item}
+                                removeBoard={removeBoard}
+                                addCard={addCard}
+                                removeCard={removeCard}
+                                handleDragEnd={handleDragEnd}
+                                handleDragEnter={handleDragEnter}
+                                updateCard={updateCard}
+                            />
+                        ))
+                    }
+                    <div className='app_boards_board'>
+                        <Editable className='app_boards_board_add'
+                                  text="Add Board" placeholder="Enter Board Title"
+                                  onSubmit={(value) => addBoard(value)}
+                        />
                     </div>
-                  </Dropdown>
-                )}
-              </div>
+                </div>
             </div>
-          </div>
-
-
-          <div className="app_outer">
-            <div className="app_boards">
-              {
-                boards.map((item) => (<Board
-                    key={item.id} board={item}
-                    removeBoard={removeBoard}
-                    addCard={addCard}
-                    removeCard={removeCard}
-                    handleDragEnd={handleDragEnd}
-                    handleDragEnter={handleDragEnter}
-                    updateCard={updateCard}
-                />
-                ))
-              }
-              <div className='app_boards_board'>
-                <Editable className='app_boards_board_add'
-                          text="Add Board" placeholder="Enter Board Title"
-                          onSubmit={(value) => addBoard(value)}
-                />
-              </div>
-            </div>
-          </div>
         </div>
     );
 };
---
+++ frontend/src/Components/Dashboard/Dropdown/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Dropdown, Space} from 'antd';
+
+const App = ({items, name, icon, minWidth}) => (
+    <div>
+        <Dropdown
+            menu={{
+                items
+            }}
+            trigger={['click']}
+            overlayStyle={{minWidth: minWidth || "250px"}}
+        >
+            <a onClick={(e) => e.preventDefault()}>
+                <Space style={{display: "flex", alignItems: "center"}}>
+                    <span style={{
+                        marginRight: '0px',
+                        marginLeft: "5px",
+                        fontWeight: "500",
+                        fontSize: '16px',
+                        cursor: 'pointer'
+                    }}>{name}</span>
+                    {icon}
+                </Space>
+            </a>
+        </Dropdown>
+    </div>
+);
+export default App;
