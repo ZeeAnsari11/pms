c9ee289c57de3bbadd1aeee5f9b1fcd65b37081f
Muhammad Asher <asher.khan@phpstudios.com>
2023-04-06 12:35:35 +0500

    SelectFields for assignees,reporter and status fields

---
+++ frontend/src/Components/Dashboard/SelectFields/GenericSelectField.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Select} from 'antd';
+import styled from 'styled-components';
+
+const {Option} = Select;
+
+const StyledSelect = styled(Select)`
+  width: 70%;
+`;
+
+const StyledOption = styled(Option)`
+  color: ${({icon}) => icon && '#1890ff'};
+  padding-left: ${({icon}) => icon && '30px'};
+`;
+
+const GenericSelectField = ({options, isMultiple, placeholder, defaultValue}) => {
+    return (
+        <StyledSelect
+            mode={isMultiple ? 'multiple' : undefined}
+            placeholder={placeholder}
+            defaultValue={defaultValue}
+        >
+            {options.map((option) => (
+                <StyledOption key={option.value} value={option.value} icon={option.icon}>
+                    {option.icon} {option.label}
+                </StyledOption>
+            ))}
+        </StyledSelect>
+    );
+};
+
+
+export default GenericSelectField;
