289afb68052094d563643fc53bfdd2632bc588ab
talha <talha.hameed@phpstudios.com>
2023-04-11 12:21:00 +0500

    Made Comments and text editor description fields

---
+++ frontend/src/Components/Dashboard/Comment/Comment.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+function Comment({ comment, index, onDelete, onEdit, selectedComment }) {
+  const [editComment, setEditComment] = useState(comment);
+
+  const handleEdit = () => {
+    onEdit(index);
+    setEditComment(comment);
+  };
+
+  const handleSave = () => {
+    onEdit(index, editComment);
+  };
+
+  const handleDelete = () => {
+    onDelete(index);
+  };
+
+  return (
+    <li key={index}>
+      <div className="comment-container">
+        <div className="avatar"></div>
+        <div className="comment-info">
+          <p className="comment-author">User {index + 1}</p>
+          {selectedComment === index ? (
+            <>
+              <input
+                type="text"
+                placeholder="Edit comment"
+                value={editComment}
+                onChange={(e) => setEditComment(e.target.value)}
+              />
+              <button onClick={handleSave}>Save</button>
+            </>
+          ) : (
+            <>
+              <p className="comment-text">{comment}</p>
+              <div className="comment-buttons">
+                <button className="comment-action-button" onClick={handleEdit}>Edit</button>
+                <button className="comment-action-button" onClick={handleDelete}>Delete</button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default Comment
