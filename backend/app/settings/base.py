bd2aa4bc3267692ef09ff4cc9136e96cc5cee953
mac-15 <mac-15@mac-15s-macbook-pro.local>
2023-04-17 16:43:31 +0500

    update docker container for prod

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -94,18 +94,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": os.environ.get("MYSQL_ENGINE", "django.db.backends.sqlite3"),
-            "NAME": os.environ.get("MYSQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-            "USER": os.environ.get("MYSQL_ROOT_USER", "root"),
-            "PASSWORD": os.environ.get("MYSQL_PASSWORD", "root"),
-            "HOST": os.environ.get("MYSQL_DATABASE_HOST", "localhost"),
-            "PORT": os.environ.get("MYSQL_DATABASE_PORT", "5432"),
-        }
-    }
-
 if DEBUG:
     DATABASES = {
         'default': {
--- backend/app/settings/production.py
+++ backend/app/settings/production.py
@@ -1,1 +1,14 @@
 from .base import *  # noqa
+
+
+if not DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("MYSQL_ENGINE", "django.db.backends.sqlite3"),
+            "NAME": os.environ.get("MYSQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+            "USER": os.environ.get("MYSQL_ROOT_USER", "root"),
+            "PASSWORD": os.environ.get("MYSQL_PASSWORD", "root"),
+            "HOST": os.environ.get("MYSQL_DATABASE_HOST", "localhost"),
+            "PORT": os.environ.get("MYSQL_DATABASE_PORT", "5432"),
+        }
+    }
