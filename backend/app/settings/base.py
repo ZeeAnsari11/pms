3438e55be83dd78d9932d19bbba6a57813fb54c0
mac-15 <usman.ilamdin@phpstudios.com>
2023-04-18 14:31:53 +0500

    Update Nginx

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -144,7 +144,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://projex.phpstudios.com',
 ]
 
 # Static files (CSS, JavaScript, Images)
