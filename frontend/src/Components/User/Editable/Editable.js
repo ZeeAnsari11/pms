31807d148d79d1aae513510288501b934b4c51bd
Mac-20 <mac-20@Mac-20s-MacBook-Pro.local>
2023-04-19 13:23:50 +0500

    Converted all the modules into Styled Component also change the structure need to fix some design issues

--- frontend/src/Components/User/Editable/Editable.js
+++ frontend/src/Components/User/Editable/Editable.js
@@ -1,8 +1,160 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
-import "./Editable.css";
 import {faCheck, faXmark} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import styled from "styled-components";
+
+const EditableWrapper = styled.div`
+  
+  .editable {
+    width: 100%;
+  }
+
+  .editable_display {
+    padding: 2px 8px;
+    color: #000;
+    cursor: pointer;
+    transition: 200ms;
+    text-align: center;
+    border-radius: 4px;
+    background-color: #f4f5f7;
+
+    &:hover {
+      background-color: #ebecf0;
+    }
+  }
+
+  .editable_edit {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .editable_edit input {
+    border: 2px solid #0079bf;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1rem;
+    padding: 10px;
+  }
+
+  .editable_edit_footer {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    flex-direction: row-reverse;
+  }
+
+  .editable_edit_footer button {
+    cursor: pointer;
+    border-radius: 5px;
+    outline: none;
+    background-color: #f5f5f5;
+    color: black;
+    border: none;
+    transition: 100ms ease;
+    padding: 10px;
+
+    &:hover {
+      background-color: #f5f5f5;
+    }
+  }
+
+  .editable_edit_footer .closeIcon {
+    cursor: pointer;
+    height: 24px;
+    width: 24px;
+  }
+`;
+
+const EditableK = styled.div`
+  width: 30%;
+`;
+
+const EditableEditK = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  input {
+    border: 2px solid #0079bf;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1rem;
+    padding: 10px;
+  }
+`;
+
+
+const EditableEditFooterK = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 8px;
+  align-items: center;
+
+  button {
+    cursor: pointer;
+    border-radius: 5px;
+    outline: none;
+    background-color: #F5F5F5;
+    color: black;
+    border: none;
+    transition: 100ms ease;
+    padding: 10px;
+
+    &:hover {
+      background-color: #EBECF0;
+    }
+  }
+
+  .closeIcon {
+    cursor: pointer;
+    height: 24px;
+    width: 24px;
+  }
+`;
+
+const HoverDiv = styled.div`
+  background-color: #ffffff;
+  padding: 1px;
+  transition: box-shadow 0.3s ease-in-out;
+  margin-bottom: 15px;
+  text-align: center;
+  margin-right: 150px;
+  border-radius: 5px;
+  position: relative;
+
+  &:hover {
+    z-index: 1;
+    background: #ebecf0;
+
+    .hover-text_k {
+      opacity: 1;
+    }
+  }
+`;
+
+const HoverText = styled.div`
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const EditableDisplay = styled.p`
+  font-size: 16px;
+  background: #ffffff;
+
+  &:hover {
+    background: #ebecf0;
+  }
+`;
+
+
+const HoverTexts = styled.span`
+  opacity: 1;
+`;
 
 function Editable(props) {
     const [isEditable, setIsEditable] = useState(false);
@@ -26,33 +178,35 @@ function Editable(props) {
     };
 
     return (
-        <div className="editable_k">
-            {isEditable ? (
-                <form className={`editable_edit_k ${props.editClass || ""}`} onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={inputText}
-                        placeholder={props.placeholder || "Add Item"}
-                        onChange={handleInputChange}
-                        autoFocus
-                    />
-                    <div className="editable_edit_footer_k">
-                        <button type="submit">{props.buttonText || <FontAwesomeIcon icon={faCheck}/>}</button>
-                        <button type="button" onClick={handleCancel}><FontAwesomeIcon icon={faXmark}/></button>
-                    </div>
-                </form>
-            ) : (
-                <div className="hover-div">
-                    <p
-                        className={`editable_display_k ${props.displayClass || ""}`}
-                        onClick={() => setIsEditable(true)}
-                    >
-                        {props.text || inputText || props.frontendText || props.frontendText}
-                    </p>
-                    <span className="hover-text_k">Click to enter</span>
-                </div>
-            )}
-        </div>
+        <EditableWrapper>
+            <EditableK>
+                {isEditable ? (
+                    <EditableEditK className={`${props.editClass || ""}`} onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={inputText}
+                            placeholder={props.placeholder || "Add Item"}
+                            onChange={handleInputChange}
+                            autoFocus
+                        />
+                        <EditableEditFooterK>
+                            <button type="submit">{props.buttonText || <FontAwesomeIcon icon={faCheck}/>}</button>
+                            <button type="button" onClick={handleCancel}><FontAwesomeIcon icon={faXmark}/></button>
+                        </EditableEditFooterK>
+                    </EditableEditK>
+                ) : (
+                    <HoverDiv>
+                        <EditableDisplay
+                            className={`${props.displayClass || ""}`}
+                            onClick={() => setIsEditable(true)}
+                        >
+                            {props.text || inputText || props.frontendText || props.frontendText}
+                        </EditableDisplay>
+                        <HoverTexts>Click to enter</HoverTexts>
+                    </HoverDiv>
+                )}
+            </EditableK>
+        </EditableWrapper>
     );
 }
 
--- frontend/src/Components/User/ForgetPassword/ForgetPassword.js
+++ frontend/src/Components/User/ForgetPassword/ForgetPassword.js
@@ -1,7 +1,85 @@
 import React, { useState } from 'react';
-import './ForgetPassword.css';
+import styled from 'styled-components';
+import   '../../../Configurations/colors';
 
-function ForgotPassword() {
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--primary-color);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  border-radius: 10px;
+  border: 2px solid var(--primary-color);
+  padding: 20px;
+  width: 510px;
+  height: 300px;
+`;
+
+const Label = styled.label`
+  margin-bottom: 10px;
+  font-weight: bold;
+  color: whitesmoke;
+  display: flex;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+
+  &:last-child {
+    margin-left: 27px;
+  }
+`;
+
+const Button = styled.button`
+  width: 220px;
+  border: none;
+  border-radius: 20px;
+  background-color: var(--primary-color);
+  color: #00000;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  border-color: whitesmoke;
+
+  &:last-child {
+    margin-left: 77px;
+  }
+
+  &:hover {
+    background-color: #000000;
+    color: #ffffff;
+    border-color: whitesmoke;
+  }
+`;
+
+const Headline = styled.h2`
+  color: whitesmoke;
+`;
+
+const Message = styled.p`
+  color: whitesmoke;
+`;
+
+const ForgotPassword = () => {
   const [email, setEmail] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
@@ -12,27 +90,24 @@ function ForgotPassword() {
   };
 
   return (
-    <div className="forgot-password-container">
-      <h2 className="forgot-password-headline">---- Forgot Password! ----</h2>
+    <Container>
+      <Headline>---- Forgot Password! ----</Headline>
       {submitted ? (
-        <p>An email has been sent to your email address with instructions on how to reset your password.</p>
+        <Message>An email has been sent to your email address with instructions on how to reset your password.</Message>
       ) : (
         <form onSubmit={handleSubmit}>
-          <label className="forgot-password-label">Email Address</label>
-          <input
-            className="forgot-password-input"
+          <Label>Email Address</Label>
+          <Input
             type="email"
             placeholder="Enter your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <button className="forgot-password-button" type="submit">
-            Reset Password
-          </button>
+          <Button type="submit">Reset Password</Button>
         </form>
       )}
-    </div>
+    </Container>
   );
-}
+};
 
 export default ForgotPassword;
