f3ecb979bf209fcdbb43cbd8dfa2a9ca5537d048
mac-15 <usman.ilamdin@phpstudios.com>
2023-05-17 22:16:43 +0500

    add smtp setting

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -62,7 +62,11 @@ REST_FRAMEWORK = {
 }
 
 DJOSER = {
-    "USER_ID_FIELD": "username"
+    "USER_ID_FIELD": "username",
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
 }
 
 ROOT_URLCONF = 'app.urls'
