972baad15ab5b58e4e21307f68ddda6c8120e89c
mac-15 <usman.ilamdin@phpstudios.com>
2023-05-22 14:31:56 +0500

    fix navigation issue

--- frontend/src/Utils/Url.js
+++ frontend/src/Utils/Url.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import React from 'react';
 import Login from "../Components/User/Login"
 import ResetPasswordPage from "../Components/User/ResetPassword/ResetPasswordPage"
