b0a038fd0422bdeaa91d098df8d678f97f63e406
Muhammad Asher <asher.khan@phpstudios.com>
2023-05-24 20:33:01 +0500

    Add Django Filter to the Viewsets

--- backend/register/views.py
+++ backend/register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from . import serializers
 from .models import Company
 
@@ -9,3 +10,5 @@ from .models import Company
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = serializers.CompanySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = "__all__"
