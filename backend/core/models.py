42b26127bc90ff316827a0130ddc46bc0c17ff3a
talha <talha.hameed@phpstudios.com>
2023-05-25 21:02:17 +0500

    - Integrate Profile and Manage Account page

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,6 +8,9 @@ from django.utils.html import format_html
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+from django.core.exceptions import ValidationError
+
 
 
 def get_default_avatar():
@@ -24,9 +27,39 @@ def get_default_avatar():
     return 'users_avatars/' + default_image
 
 
+
+def validate_date_format(value):
+    try:
+        datetime.datetime.strptime(value, '%Y-%m-%d')
+    except ValueError:
+        raise ValidationError('Invalid date format. The date must be in YYYY-MM-DD format.')
+
+
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to='users_avatars', default=get_default_avatar)
+#
+#     def image_tag(self):
+#         return format_html('<img src="{}" style="width: 100px; height: 100px;" />'.format(self.image.url))
+#
+#     image_tag.short_description = 'Image Thumbnail'
+#
+#     def __str__(self):
+#         return f'{self.user.username} Profile Avatar'
+#
+#
+# @receiver(post_save, sender=User)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='users_avatars', default=get_default_avatar)
+    company = models.ForeignKey('register.Company', on_delete=models.CASCADE, default=1)
+    department = models.CharField(max_length=100, default='')
+    job_title = models.CharField(max_length=100, default='')
+    joining_date = models.DateField(auto_now_add=True)
 
     def image_tag(self):
         return format_html('<img src="{}" style="width: 100px; height: 100px;" />'.format(self.image.url))
