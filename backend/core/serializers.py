0263510c4ef7a88af543e74ce01ae3610d4a6ea7
talha <talha.hameed@phpstudios.com>
2023-05-25 21:02:17 +0500

    - Integrate Profile and Manage Account page

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import UserProfile
 from django.contrib.auth.models import User
+from register.serializers import CompanySerializer
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -9,10 +10,17 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'last_login', 'is_superuser', 'email', 'is_staff', 'is_active', 'user_permissions']
 
 
+# class UserProfileSerializer(serializers.ModelSerializer):
+#     user = CustomUserSerializer(read_only=True)
+#
+#     class Meta:
+#         model = UserProfile
+#         fields = ('image', 'user')
+
 class UserProfileSerializer(serializers.ModelSerializer):
     user = CustomUserSerializer(read_only=True)
+    company = CompanySerializer()  # Assuming you have defined the CompanySerializer in the 'register' app
 
     class Meta:
         model = UserProfile
-        fields = ('image', 'user')
-
+        fields = ('image', 'user', 'company', 'department', 'job_title', 'joining_date')
