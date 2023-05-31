d4d2ffa9aef3f2a96cb20ecca76ce3fbfaad4e7e
Muhammad Asher <asher.khan@phpstudios.com>
2023-05-31 13:50:42 +0500

    Combine Project and Dashbaord sidebar into one

--- frontend/src/Components/Dashboard/Sidebar/index.jsx
+++ frontend/src/Components/Dashboard/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {NavLink, useLocation, useParams} from 'react-router-dom';
 import {ProjectCategoryCopy} from './constants/projects';
@@ -16,14 +16,66 @@ import {
     LinkText,
     NotImplemented,
 } from './Styles';
+import axios from "axios";
 
-const propTypes = {
-    project: PropTypes.object.isRequired,
-};
 
-const ProjectSidebar = ({project}) => {
+const ProjectSidebar = () => {
     const match = useLocation();
     const {projectId} = useParams()
+
+    let authToken = localStorage.getItem('auth_token')
+    const [projectData, setProjectData] = useState({});
+    const [icon, setIcon] = useState(null);
+
+    const defaultIconPath = "Images/NoImage.jpeg"
+
+    let IconPath = projectData.icon
+    if (IconPath != null) {
+        IconPath = `${process.env.REACT_APP_HOST}/${icon}`
+    } else {
+        IconPath = 'http://localhost:3000/Images/NoImage.jpeg'
+    }
+    const [name, setName] = useState(''); // Set initial value from project object
+    const [key, setKey] = useState(''); // Set initial value from project object
+    const [projectCategory, setProjectCategory] = useState(''); // Set initial value from project object
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const response = await axios.get(`${process.env.REACT_APP_HOST}/api/projects/${projectId}`, {
+                headers: {
+                    Authorization: `Token ${authToken}`,
+                },
+            });
+            setProjectData(response.data);
+
+        };
+
+        fetchProjects();
+    }, []);
+
+    useEffect(() => {
+        if (projectData.icon != null) {
+            setIcon(projectData.icon)
+        }
+
+        if (projectData.icon == null) {
+            setIcon(defaultIconPath)
+        }
+    }, [projectData]);
+
+    useEffect(() => {
+        if (projectData.name) {
+            setName(projectData.name);
+            setKey(projectData.key);
+            setProjectCategory(projectData.project_category)
+        }
+    }, [projectData]);
+
+    const project = {
+        name: name,
+        category: projectCategory.project_category,
+        icon: IconPath
+    }
 
 
     return (
@@ -36,14 +88,24 @@ const ProjectSidebar = ({project}) => {
                 </ProjectTexts>
             </ProjectInfo>
 
-            {renderLinkItem(match, 'Kanban Board', 'kanban', `/project-views/${projectId}/dashboard`)}
-            {renderLinkItem(match, 'Project Settings', 'settings', `/project-views/${projectId}/project-setting`)}
+            {renderLinkItem(match, 'Kanban Board', 'kanban', `/project/${projectId}/dashboard`)}
+            {renderLinkItem(match, 'Project Settings', 'settings', `/project/${projectId}/project-setting`)}
+            <Divider/>
+            {renderLinkItem(match, 'Summary', 'Summary', `/project/${projectId}/setting/summary`)}
+            {renderLinkItem(match, 'Notifications', 'Notifications', `/project/${projectId}/setting/notification`)}
+            {renderLinkItem(match, 'Integrations', 'Integrations', `/project/${projectId}/setting/integrations`)}
+            {renderLinkItem(match, 'Tags', 'Tags', `/project/${projectId}/setting/tags`)}
+            {renderLinkItem(match, 'Columns', 'Columns', `/project/${projectId}/setting/columns`)}
+            {renderLinkItem(match, 'Permissions', 'Permissions', `/project/${projectId}/setting/permissions`)}
+            {renderLinkItem(match, 'Close Project', 'Close Project', `/project/${projectId}/close-project`)}
             <Divider/>
             {renderLinkItem(match, 'Roadmap', 'roadmap')}
             {renderLinkItem(match, 'Releases', 'release')}
             {renderLinkItem(match, 'Code', 'code')}
             {renderLinkItem(match, 'Project Pages', 'pages')}
             {renderLinkItem(match, 'Reports', 'reports')}
+            <Divider/>
+
         </Sidebar>
     );
 };
@@ -64,6 +126,5 @@ const renderLinkItem = (match, text, iconType, path) => {
     );
 };
 
-ProjectSidebar.propTypes = propTypes;
 
 export default ProjectSidebar;
