89debd35b49c94beb8d030b4121da088e0c8765b
mac-15 <usman.ilamdin@phpstudios.com>
2023-06-27 21:00:51 +0500

    add the user manage page

--- frontend/src/Components/User/Manage/ManageUsers.jsx
+++ frontend/src/Components/User/Manage/ManageUsers.jsx
@@ -1,251 +1,255 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import NavBar from "../../Dashboard/Navbar";
-import UserSidebar from "../../Dashboard/Sidebar/UserSidebar";
+import NavBar from '../../Dashboard/Navbar/index';
+import apiRequest from '../../../Utils/apiRequest';
+import { AuthContext } from '../../../Utils/AuthContext';
+import { toast, ToastContainer } from "react-toastify";
 import GenericSelectField from "../../Dashboard/SelectFields/GenericSelectField";
-import {Link} from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import { StatusCodes } from "http-status-codes";
+import { AxiosError } from "axios";
+import { Table, Input, Button, Modal, Pagination, Space, Form, Switch } from 'antd';
+import { AiOutlineSetting, AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import UserSidebar from "../../Dashboard/Sidebar/UserSidebar";
+
+const { Column } = Table;
+const {userModalForm} = Form;
 
 
-const PersonalSetting = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
+const PermissionsContainer = styled.div`
+    margin-left: 16%;
+    margin-top: 0%;
+    padding-top: 61px;
+    padding-left: 80px;
+    margin-right: 90px;
 `;
 
-const PageWrapper = styled.div`
-  background-color: #fff;
-  height: 100vh;
-  margin-left: 50px;
-  padding: 40px 20% 0 14%;
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 10px 20px 0;
-`;
-
-const Details = styled.h1`
-  margin: 0;
-`;
-
-const CardInfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  width: 60%;
-  //padding-bottom: 5px;
-`;
-
-const CardInfoBoxTitle = styled.div`
-  font-weight: bold;
-  font-size: 1.3rem;
-  display: flex;
-  gap: 10px;
-  align-items: center;
-  margin-top: 10px;
-`;
-
-const TaskList = styled.div`
-  margin: 8px 0 15px;
-`;
-
-const ConfigureMessage = styled.p`
-  margin-top: -5px;
-  font-size: 0.9rem;
-`;
-
-const CheckboxWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-  margin-top: -5px;
-`;
-
-const CheckboxLabel = styled.label`
-  margin-bottom: 7px;
-`;
-
-const FormWrapper = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
+const Heading = styled.h2`
+    margin-left: 10px;
 `;
 
 
-const SaveButton = styled.button`
-  background-color: #0062FF;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  font-size: 0.8rem;
-  font-weight: bold;
-  padding: 0.5rem 1rem;
-  margin-top: 1rem;
-  cursor: pointer;
-  transition: background-color 0.3s ease-in-out;
-  width: fit-content;
-  margin-bottom: 15px;
 
-  &:hover {
-    background-color: #3e81ed;
-  }
+const PermissionsTable = styled(Table)`
+    margin-top: 20px;
 `;
 
-const issues = [
-    {value: 'option1', label: 'Enabled'},
-    {value: 'option2', label: 'Disabled'},
-    {value: 'option3', label: 'Inherit from global settings'},
-];
+const PaginationWrapper = styled.div`
+  float: right;
+  margin-top: 15px;
+`;
 
-const homepage = [
-    {value: 'option1', label: 'Your work'},
-    {value: 'option2', label: 'Projects directory'},
-    {value: 'option3', label: 'Dashboards'},
-];
 
-const email = [
-    {value: 'option1', label: 'Send me email notifications'},
-    {value: 'option2', label: 'Do not send me email notifications'},
-];
 
-const notificationFormat = [
-    {value: 'option1', label: 'HTML'},
-    {value: 'option2', label: 'Text'},
-];
+const ManageUsers = () => {
+    const [data, setData] = useState([]); // Data for the table
+    const [filteredData, setFilteredData] = useState([]); // Filtered data based on search query
+    const [searchQuery, setSearchQuery] = useState(''); // Search query
+    const [currentPage, setCurrentPage] = useState(1); // Current page number
+    const [pageSize, setPageSize] = useState(10); // Number of items per page
+    const [totalItems, setTotalItems] = useState(0); // Total number of items
+    const [modalVisible, setModalVisible] = useState(false); // Modal visibility
+    const [modalData, setModalData] = useState(null); // Data for the modal
+    const [modalEditedData, setModalEditedData] = useState(null); // Data for the modal
 
-function ManageUsers() {
+
+    const { authToken } = useContext(AuthContext);
+
+    const { projectId } = useParams();
+
+    // Mock API call to fetch data
+    const fetchData = () => {
+        // Replace this with your actual API call to fetch data
+
+        const response = [
+            { id: 1, username: 'john123', email: 'john@example.com', firstName: 'John', lastName: 'Doe', isStaff: true },
+            { id: 2, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 3, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 4, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 5, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 6, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 7, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 8, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 9, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 10, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 11, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 12, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 13, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 14, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 15, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 16, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            { id: 17, username: 'jane456', email: 'jane@example.com', firstName: 'Jane', lastName: 'Smith', isStaff: false },
+            // Add more data...
+        ];
+
+        setData(response);
+        setFilteredData(response);
+        setTotalItems(response.length);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const layout = {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 16 },
+    };
+
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+
+        // Filter the data based on search query
+        const filtered = data.filter((item) =>
+            item.username.toLowerCase().includes(query.toLowerCase()) ||
+            item.email.toLowerCase().includes(query.toLowerCase()) ||
+            item.firstName.toLowerCase().includes(query.toLowerCase()) ||
+            item.lastName.toLowerCase().includes(query.toLowerCase())
+        );
+        setFilteredData(filtered);
+        setTotalItems(filtered.length);
+        setCurrentPage(1);
+    };
+
+      // Handle page change
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handlePageSizeChange = (current, size) => {
+        setPageSize(size);
+        setCurrentPage(1);
+    };
+
+    const handleDelete = (id) => {
+        const updatedData = data.filter((item) => item.id !== id);
+            setData(updatedData);
+            setFilteredData(updatedData);
+            setTotalItems(updatedData.length);
+    };
+
+    // Handle edit action
+    const handleEdit = (record) => {
+        setModalData(record);
+        setModalVisible(true);
+    };
+
+  // Handle modal close
+    const handleModalClose = () => {
+        setModalVisible(false);
+        setModalData(null);
+    };
+
+    const handleModalSave = () => {
+        console.log('Modal data before edit', modalData);
+        setModalVisible(false);
+        setModalData(null);
+    };
+
+
+
+    const columns = [
+    { title: 'ID', dataIndex: 'id' },
+    { title: 'Username', dataIndex: 'username' },
+    { title: 'Email', dataIndex: 'email' },
+    { title: 'First Name', dataIndex: 'firstName' },
+    { title: 'Last Name', dataIndex: 'lastName' },
+    { title: 'Is Staff', dataIndex: 'isStaff', render: (isStaff) => (isStaff ? 'Yes' : 'No') },
+    {
+        title: 'Actions',
+            render: (_, record) => (
+            <Space>
+                <Button type="link" onClick={() => handleEdit(record)}>
+                    <AiOutlineEdit /> Edit
+                </Button>
+                <Button type="link" onClick={() => handleDelete(record.id)}>
+                    <AiOutlineDelete /> Delete
+                </Button>
+            </Space>
+            ),
+        },
+    ];
+
+    // Calculate pagination values
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedData = filteredData.slice(startIndex, endIndex);
+
+
 
     return (
-        <PersonalSetting>
-            <NavBar/>
+        <>
             <UserSidebar/>
-            <PageWrapper>
-                <Header>
-                    <Details>Personal Settings</Details>
-                </Header>
-                <FormWrapper>
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Your time zone
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={issues}
-                            isMultiple={false}
-                            defaultValue={"Asia/Karachi"}
-                            isDisabled={true}/>
-                    </TaskList>
-                </CardInfoBox>
-                <ConfigureMessage>Configure your timezone settings in your <Link to="/profile">profile settings
-                    page</Link></ConfigureMessage>
+            <NavBar/>
+            <PermissionsContainer>
+                <Heading>Users List</Heading>
+                <Input.Search placeholder="Search" value={searchQuery} onChange={handleSearch} style={{ marginBottom: 16 }} />
+                <PermissionsTable
+                    dataSource={paginatedData}
+                    columns={columns}
+                    pagination={false}
+                    rowKey="id"
+                    />
+                <PaginationWrapper>
+                    <Pagination
+                        current={currentPage}
+                        pageSize={pageSize}
+                        total={totalItems}
+                        onChange={handlePageChange}
+                        showSizeChanger
+                        onShowSizeChange={handlePageSizeChange}
+                        style={{ marginBottom: 16 }}
+                />
+                </PaginationWrapper>
+            </PermissionsContainer>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
 
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Language
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={issues}
-                            isMultiple={false}
-                            defaultValue={"English (United Kingdom)"}
-                            isDisabled={true}/>
-                    </TaskList>
-                </CardInfoBox>
-                <ConfigureMessage>Configure your language settings in your <Link to="/profile">profile settings
-                    page</Link></ConfigureMessage>
-
-
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Watch your issues
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={issues}
-                            isMultiple={false}
-                            defaultValue={"Inherit from global settings"}/>
-                    </TaskList>
-                </CardInfoBox>
-                <ConfigureMessage>If you select enabled, you will automatically 'watch' the issues you interact
-                    with.</ConfigureMessage>
-
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Your Jira homepage
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={homepage}
-                            isMultiple={false}
-                            defaultValue={"Your work"}/>
-                    </TaskList>
-                </CardInfoBox>
-                <ConfigureMessage>You'll see this page when you log in or select the Jira logo.</ConfigureMessage>
-
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Email notification for issue activity
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={email}
-                            isMultiple={false}
-                            placeholder={"Unassigned"}
-                            defaultValue={"Send me email notifications"}/>
-                    </TaskList>
-                    <ConfigureMessage>Get email updates for issue activity when:</ConfigureMessage>
-                    <CheckboxWrapper>
-                        <CheckboxLabel htmlFor="watching">
-                            <input type="checkbox" defaultChecked={true} id="watching"/>
-                            You're <strong>watching</strong> the issue
-                        </CheckboxLabel>
-
-                        <CheckboxLabel htmlFor="reporter">
-                            <input type="checkbox" defaultChecked={true} id="reporter"/>
-                            You're the <strong>reporter</strong>
-                        </CheckboxLabel>
-
-                        <CheckboxLabel htmlFor="assignee">
-                            <input type="checkbox" defaultChecked={true} id="assignee"/>
-                            You're the <strong>assignee</strong> for the issue
-                        </CheckboxLabel>
-
-                        <CheckboxLabel htmlFor="mention">
-                            <input type="checkbox" defaultChecked={true} id="mention"/>
-                            Someone <strong>mentions</strong> you
-                        </CheckboxLabel>
-
-                        <CheckboxLabel htmlFor="changes">
-                            <input type="checkbox" id="changes"/>
-                            You <strong>make changes</strong> to the issue
-                        </CheckboxLabel>
-                    </CheckboxWrapper>
-                    <ConfigureMessage>You may also receive other email notifications like those configured by your Jira
-                        admin and updates for filter subscriptions.</ConfigureMessage>
-                    <ConfigureMessage><Link to="/profile">Learn more about email notifications</Link></ConfigureMessage>
-                </CardInfoBox>
-
-                <CardInfoBox>
-                    <CardInfoBoxTitle>
-                        Email notifications format
-                    </CardInfoBoxTitle>
-                    <TaskList>
-                        <GenericSelectField
-                            options={notificationFormat}
-                            isMultiple={false}
-                            defaultValue={"HTML"}/>
-                    </TaskList>
-                </CardInfoBox>
-                <SaveButton>Save Changes</SaveButton>
-            </FormWrapper>
-
-            </PageWrapper>
-        </PersonalSetting>
+            <Modal
+                title="Edit User"
+                open={modalVisible}
+                onOk={handleModalSave}
+                onCancel={handleModalClose}
+                footer={[
+                    <Button form="myForm" key="submit" htmlType="submit">
+                        Submit
+                    </Button>
+        ]}
+            >
+            {modalData && (
+                <Form initialValues={modalData}
+                >
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please enter a username' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please enter an email' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="First Name" name="firstName" rules={[{ required: true, message: 'Please enter a first name' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Last Name" name="lastName" rules={[{ required: true, message: 'Please enter a last name' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Is Staff" name="isStaff" valuePropName="checked">
+                        <Switch />
+                    </Form.Item>
+                </Form>
+            )}
+            </Modal>
+        </>
     );
-}
-
+};
 
 export default ManageUsers;
