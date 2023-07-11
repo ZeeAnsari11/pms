5a0901cb1f9ffc4e1310635137ba6f51e2dfb96f
Muhammad Asher <asher.khan@phpstudios.com>
2023-07-11 20:55:30 +0500

    Comment, Worklog and Time Tracking Integration

--- backend/projects/filters.py
+++ backend/projects/filters.py
@@ -1,17 +1,34 @@
 from django_filters.rest_framework import FilterSet
-from .models import Issue
+from .models import Issue, WorkLog
 
 
 class IssueFilter(FilterSet):
     class Meta:
         model = Issue
         fields = {
+            'slug': ['iexact'],
             'name': ['iexact'],
             'priority': ['iexact'],
             'assignee__username': ['iexact'],
             'reporter__username': ['iexact'],
             'project__name': ['iexact'],
+            'project__id': ['iexact'],
             'type__type': ['iexact'],
             'status__status': ['iexact'],
 
+        }
+
+
+class WorklogFilter(FilterSet):
+    class Meta:
+        model = WorkLog
+        fields = {
+            'time_spent': ['iexact'],
+            'comment': ['iexact'],
+            'user__username': ['iexact'],
+            'issue__id': ['iexact'],
+            'issue__slug': ['iexact'],
+            'issue__project__id': ['iexact'],
+            'created_at': ['iexact'],
+            'updated_at': ['iexact'],
         }
--- frontend/src/Components/Dashboard/TimeTracking/index.jsx
+++ frontend/src/Components/Dashboard/TimeTracking/index.jsx
@@ -1,17 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
 import {Progress, Modal, DatePicker, TimePicker, InputNumber, Input} from 'antd';
 import moment from 'moment';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import {TimeTrackingContainer, ProgressBarContainer, InputHeading, ModalContent} from './styles'
+import {TimeTrackingContainer, ProgressBarContainer, InputHeading, ModalContent, StyledSpan} from './styles'
 import Worklog from '../Worklog/Worklog';
+import EstimateTimer from "../EstimateTimer/EstimateTimer";
+import axios from 'axios';
 
 const TimeTracking = ({OrginalEstimate}) => {
+    let authToken = localStorage.getItem('auth_token')
+
+
     const [originalEstimate, setOriginalEstimate] = useState(OrginalEstimate);
+    const [currentIssueid, setCurrentIssueid] = useState('');
     const [timeSpent, setTimeSpent] = useState(0);
     const [timeRemaining, setTimeRemaining] = useState(0);
-    const [startDate, setStartDate] = useState(null);
-    const [startTime, setStartTime] = useState(null);
+    const [startDate, setStartDate] = useState('');
+    const [startTime, setStartTime] = useState('');
     const [workDescription, setWorkDescription] = useState("");
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [worklogs, setWorklogs] = useState([]);
@@ -20,12 +27,123 @@ const TimeTracking = ({OrginalEstimate}) => {
         setIsModalVisible(true);
     };
 
-    const handleModalOk = () => {
-        const newWorklog = {
-            username: "Alice", // replace with actual username
-            timeTracked: timeSpent,
-            description: workDescription
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedIssueSlug = urlParams.get('selectedIssue');
+    console.log("selectedIssueSlug", selectedIssueSlug);
+
+    const {issueId, projectId} = useParams();
+
+
+    useEffect(() => {
+        const getCurrentIssueId = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_HOST}/api/issues/?slug__iexact=${selectedIssueSlug}`, {
+                    headers: {
+                        Authorization: `Token ${authToken}`,
+                    },
+                });
+                setCurrentIssueid(response.data[0].id);
+            } catch (error) {
+                console.log(error);
+                throw new Error('Failed to fetch Current Issue Id');
+            }
         };
+
+        if (selectedIssueSlug) {
+            getCurrentIssueId();
+        } else {
+            setCurrentIssueid(issueId);
+        }
+    }, [selectedIssueSlug, issueId]);
+
+    useEffect(() => {
+        if (timeRemaining === 0) {
+            setTimeRemaining(originalEstimate)
+        } else {
+            setTimeRemaining(timeRemaining);
+        }
+    }, [timeRemaining]);
+
+
+    useEffect(() => {
+        const fetchWorklogs = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_HOST}/api/worklogs/?issue=${currentIssueid}`, {
+                    headers: {
+                        Authorization: `Token ${authToken}`,
+                    },
+                });
+                setWorklogs(response.data);
+            } catch (error) {
+                console.log(error);
+                throw new Error('Failed to fetch comments');
+            }
+        };
+
+        fetchWorklogs();
+    }, []);
+
+
+    useEffect(() => {
+        const fetchWorklogs = async () => {
+            try {
+                if (currentIssueid) {
+                    const response = await axios.get(`${process.env.REACT_APP_HOST}/api/worklogs/?issue=${currentIssueid}`, {
+                        headers: {
+                            Authorization: `Token ${authToken}`,
+                        },
+                    });
+                    setWorklogs(response.data);
+                }
+            } catch (error) {
+                console.log(error);
+                throw new Error('Failed to fetch comments');
+            }
+        };
+
+        fetchWorklogs();
+    }, [currentIssueid]);
+
+
+    useEffect(() => {
+        let totalSpent = 0;
+        worklogs.forEach(worklog => {
+            totalSpent += worklog.time_spent;
+        });
+        const remainingTime = originalEstimate - totalSpent;
+        setTimeRemaining(remainingTime);
+        setTimeSpent(totalSpent);
+    }, [worklogs]);
+
+
+    const handleModalOk = () => {
+
+        const config = {
+            headers: {
+                Authorization: `Token ${authToken}`
+            }
+        };
+
+        const newWorklog = {
+            time_spent: timeSpent,
+            comment: workDescription,
+            issue_id: currentIssueid,
+            date: startDate.format('YYYY-MM-DD'),
+            time: startTime.format('HH:mm:ss'),
+        };
+
+        // Make the POST request
+        axios.post(`${process.env.REACT_APP_HOST}/api/worklogs/`, newWorklog, config)
+
+            .then(response => {
+                // Handle the response if needed
+                console.log(response.data);
+            })
+            .catch(error => {
+                // Handle the error if the request fails
+                console.error(error);
+            });
+
         setWorklogs([...worklogs, newWorklog]);
         setIsModalVisible(false);
     };
@@ -36,29 +154,39 @@ const TimeTracking = ({OrginalEstimate}) => {
 
     const handleTimeSpentChange = (value) => {
         setTimeSpent(value);
+        const remainingTime = originalEstimate - value;
+        setTimeRemaining(remainingTime <= 0 ? '0m' : remainingTime);
     };
 
     const handleTimeRemainingChange = (value) => {
         setTimeRemaining(value);
+        const remainingTime = originalEstimate - value;
+        setTimeRemaining(remainingTime <= 0 ? '0m' : remainingTime);
     };
 
     const handleOriginalEstimateTimeChange = (value) => {
         setOriginalEstimate(value);
     };
-    const handleDateChange = (date, dateString) => {
-        setStartDate(dateString);
+    const handleDateChange = (date) => {
+        setStartDate(date);
     };
 
-    const handleTimeChange = (time, timeString) => {
-        setStartTime(timeString);
+
+    const handleWorklogDescription = (desc) => {
+        setWorkDescription(desc)
     };
+
+    const handleTimeChange = (time) => {
+        setStartTime(time);
+    };
+
 
     const getTimePercentage = () => {
         let totalTime;
         if (!timeRemaining && originalEstimate) {
             totalTime = originalEstimate;
         } else {
-            totalTime = timeSpent + timeRemaining;
+            totalTime = timeSpent + originalEstimate;
         }
         return Math.round((timeSpent / totalTime) * 100);
     };
@@ -66,7 +194,7 @@ const TimeTracking = ({OrginalEstimate}) => {
     const getTimeLogged = () => {
         const hours = Math.floor(timeSpent);
         if (hours > 0) {
-            return `${hours}h logged`;
+            return `${convertToTimeFormat(hours)} logged`;
         } else {
             return "No time logged";
         }
@@ -75,13 +203,41 @@ const TimeTracking = ({OrginalEstimate}) => {
     const getTimeRemaining = () => {
         const hours = Math.floor(timeRemaining);
         if (timeRemaining) {
-            return `${hours}h remaining`;
+            return `${convertToTimeFormat(hours)} remaining`;
         } else if (originalEstimate) {
-            return `${originalEstimate}h estimated`;
+            return `${convertToTimeFormat(originalEstimate)} estimated`;
         } else {
             return "";
         }
     };
+
+    function convertToTimeFormat(hours) {
+        if (typeof hours === 'string') {
+            return hours;
+        }
+        const weeks = Math.floor(hours / 40);
+        hours %= 40;
+        const days = Math.floor(hours / 8);
+        hours %= 8;
+        const minutes = Math.ceil((hours - Math.floor(hours)) * 60);
+        hours = Math.floor(hours);
+
+        let timeFormat = '';
+        if (weeks > 0) {
+            timeFormat += `${weeks}w `;
+        }
+        if (days > 0) {
+            timeFormat += `${days}d `;
+        }
+        if (hours > 0) {
+            timeFormat += `${hours}h `;
+        }
+        if (minutes > 0) {
+            timeFormat += `${minutes}m`;
+        }
+
+        return timeFormat.trim();
+    }
 
 
     return (
@@ -89,10 +245,12 @@ const TimeTracking = ({OrginalEstimate}) => {
             <TimeTrackingContainer>
                 <div>
                 <span style={{fontWeight: "bold", marginRight: "10px"}}>
-          Original Estimate (hours):
+          Original Estimate
                 </span>
-                    <InputNumber size={"small"} min={0} value={originalEstimate}
-                                 onChange={handleOriginalEstimateTimeChange}/>
+                    <StyledSpan
+                    >{convertToTimeFormat(originalEstimate)}</StyledSpan>.
+                    {/*<InputNumber size={"small"} min={0} value={originalEstimate}*/}
+                    {/*             onChange={handleOriginalEstimateTimeChange}/>*/}
                 </div>
                 <ProgressBarContainer>
                     <Progress style={{marginTop: "20px"}} percent={getTimePercentage()}
@@ -117,32 +275,44 @@ const TimeTracking = ({OrginalEstimate}) => {
                         <div><p style={{marginTop: 0, fontWeight: "bold"}}>{getTimeLogged()}</p></div>
                         <div><p style={{marginTop: 0, fontWeight: "bold"}}>{getTimeRemaining()}</p></div>
                     </div>
+
+                    <div
+                        style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+                        <div><p
+                            style={{alignSelf: "center", fontSize: "1rem", marginTop: 0, fontWeight: "500"}}>The
+                            original estimate for
+                            this issue was <StyledSpan
+                            >{convertToTimeFormat(originalEstimate)}</StyledSpan>.
+                        </p></div>
+                    </div>
+
                     <ModalContent style={{marginTop: "20px"}}>
 
                         <div>
-                            <InputHeading>Time spent (hours):</InputHeading>
-                            <InputNumber min={0} value={timeSpent}
-                                         onChange={handleTimeSpentChange}/>
+                            <InputHeading>Time spent</InputHeading>
+                            <EstimateTimer onHoursChange={handleTimeSpentChange}/>
                         </div>
                         <div>
-                            <InputHeading>Time remaining (hours):
+                            <InputHeading>Time remaining
                             </InputHeading>
-                            <InputNumber min={0} value={timeRemaining} onChange={handleTimeRemainingChange}/>
+                            <StyledSpan>{convertToTimeFormat(timeRemaining)}</StyledSpan>
                         </div>
+
                         <div>
                             <InputHeading>Date:</InputHeading>
-                            <DatePicker onChange={handleDateChange}/>
+                            <DatePicker value={startDate} onChange={handleDateChange}/>
                         </div>
                         <div>
                             <InputHeading>Time:</InputHeading>
-                            <TimePicker use12Hours format="h:mm a" defaultValue={moment()} onChange={handleTimeChange}/>
+                            <TimePicker use12Hours format="h:mm a" value={startTime}
+                                        onChange={handleTimeChange}/>
                         </div>
 
                     </ModalContent>
                     {timeSpent ? (
                         <div style={{marginTop: "10px", marginBottom: "10px"}}>
                             <InputHeading>Work Description:</InputHeading>
-                            <ReactQuill value={workDescription} onChange={setWorkDescription}
+                            <ReactQuill value={workDescription} onChange={handleWorklogDescription}
                             />
                         </div>
                     ) : null}
