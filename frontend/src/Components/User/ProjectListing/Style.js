d0ec7304a4f49beb30793dc8cca7fbb84b2e0bfd
Zeeshan Bashi <zeeshan.bscs231@gmail.com>
2023-07-23 22:21:21 +0500

    add the project listing page styling

--- frontend/src/Components/User/ProjectListing/Style.js
+++ frontend/src/Components/User/ProjectListing/Style.js
@@ -1,160 +1,31 @@
 import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import {Link} from 'react-router-dom';
 
-export const ProjectListingTable = styled.table`
+export const ProjectListingTable = styled.div`
   width: 100%;
-  border-collapse: collapse;
-
-  th {
-    text-align: left;
-    font-weight: bold;
-    padding: 12px 8px;
-    border-bottom: 2px solid #ddd;
-  }
-
-  td {
-    padding: 12px 8px;
-  }
-
-  tr:not(:first-child) {
-    border-top: 1px solid #ddd;
-  }
-
-  tr:hover {
-    background-color: #EBECF0;
-  }
-
-  .name-column {
-    display: flex;
-    align-items: center;
-  }
-
-  .name-column img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-
-  .option-column-align {
-    margin-right: 45%;
-  }
-
-  .star-column {
-    color: #ccc;
-    cursor: pointer;
-    transition: color 0.2s ease-in-out;
-  }
-
-  .star-column:hover {
-    color: #ffd700;
-  }
-
-  .lead-column {
-    display: flex;
-    align-items: center;
-  }
-
-  .lead-column img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-
-  .options-column {
-    text-align: center;
-    cursor: pointer;
-    justify-content: center;
-  }
-
-  .options-column:hover {
-    text-decoration: underline;
-  }
+  margin-top: 5px;
 `;
 
 
-export const NameColumn = styled.td`
+export const ProjectLink = styled(Link)`
   display: flex;
   align-items: center;
-
-  a {
-    color: #000;
-    text-decoration: none;
-    position: relative;
-
-    &:hover {
-      color: #1E64D1;
-      text-decoration: underline;
-
-      &:after {
-        content: "";
-        position: absolute;
-        left: 0;
-        bottom: -2px;
-        width: 100%;
-        height: 2px;
-        background-color: #1E64D1;
-        transition: transform 0.2s ease-in-out;
-        transform: scaleX(0);
-        transform-origin: left;
-      }
-
-      &:before {
-        content: "";
-        position: absolute;
-        left: 0;
-        bottom: -2px;
-        width: 100%;
-        height: 2px;
-        background-color: #1E64D1;
-        transform: scaleX(1);
-        transform-origin: right;
-        transition: transform 0.2s ease-in-out;
-      }
-    }
-  }
-
-  img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-
-  span {
-    display: inline-block;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: calc(100% - 32px);
-  }
 `;
 
+export const ProjectIcon = styled.span`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-right: 8px;
+`;
 
 export const ProjectAvatar = styled.img`
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
-  margin-right: 8px;
-`;
-
-export const ProjectName = styled.span`
-`;
-
-
-export const ProjectAvatarWrapper = styled.div`
-  width: 24px;
-  height: 24px;
-  margin-right: 8px;
-`;
-export const OptionsColumn = styled.th`
-  text-align: center;
-  cursor: pointer;
-  justify-content: center;
-
-  &:hover {
-    text-decoration: underline;
-  }
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 export const modalStyle = {
@@ -171,46 +42,64 @@ export const NoProjectsMessage = styled.tr`
   }
 `;
 
-export const StarColumn = styled.span`
-  margin-right: 5px;
-  color: #ccc;
-  cursor: pointer;
-  transition: color 0.2s ease-in-out;
 
-  &:hover {
-    color: #ffd700;
-  }
-`;
-export const StarSelect = styled.td`
+export const SearchInputContainer = styled.div`
   display: flex;
+  flex-direction: row;
   align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  max-width: 300px;
+  position: relative;
 
-  img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    margin-right: 8px;
+  @media (min-width: 768px) {
+    max-width: 400px;
   }
 `;
 
-export const LeadColumn = styled.td`
-  display: flex;
-  align-items: center;
-
-  img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-`;
-
-export const OptionsColumns = styled.td`
+export const SearchInput = styled.input`
+  width: 100%;
+  padding: 10px 40px 10px 30px;
+  font-size: 16px;
+  border: 2px solid #CCCCCC;
+  border-radius: 4px;
+  margin-right: 10px;
   text-align: center;
-  cursor: pointer;
-  justify-content: center;
 
   &:hover {
-    text-decoration: underline;
+    border-color: #005CA9;
+    background: #EBECF0;
+  }
+
+  @media (min-width: 768px) {
+    padding: 10px 50px 10px 30px;
+  }
+`;
+
+export const SearchIcon = styled(FaSearch)`
+  position: absolute;
+  left: 1px;
+  flex-direction: row-reverse;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 18px;
+  color: #999999;
+  @media (min-width: 768px) {
+    font-size: 24px;
+    left: 20px;
+  }
+`;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin-bottom: 20px;
+  position: relative;
+
+  @media (min-width: 768px) {
+    margin-bottom: 40px;
   }
 `;
