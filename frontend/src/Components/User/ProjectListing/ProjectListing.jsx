cd7a8f22cd69cea9377cd18bbbbeb5525ea507d6
Muhammad Asher <asher.khan@phpstudios.com>
2023-07-26 18:31:15 +0500

    Store Projects Data in Redux Store

--- frontend/src/Components/User/ProjectListing/ProjectListing.jsx
+++ frontend/src/Components/User/ProjectListing/ProjectListing.jsx
@@ -1,23 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import apiRequest from '../../../Utils/apiRequest';
-import { Space, Table, Tag } from 'antd';
+import {Space, Table, Tag} from 'antd';
 import * as ProjectListingComponents from './Style';
 import {Modal} from 'antd';
 import {GrAlert} from 'react-icons/gr';
 import ToastContainer from "../../../Shared/Components/Toast";
-import { displayErrorMessage } from "../../../Shared/notify";
+import {displayErrorMessage} from "../../../Shared/notify";
+import {useSelector} from "react-redux";
 
 const ProjectListing = () => {
     const columns = [
-        { title: 'ID', dataIndex: 'id' },
+        {title: 'ID', dataIndex: 'id'},
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
             render: (text, record) => (
-                <ProjectListingComponents.ProjectLink to={`/project/${ record.id }/dashboard`}>
+                <ProjectListingComponents.ProjectLink to={`/project/${record.id}/dashboard`}>
                     <ProjectListingComponents.ProjectIcon>
-                        <ProjectListingComponents.ProjectAvatar src={`${process.env.REACT_APP_HOST}/${record.icon}`} alt={record.name} />
+                        <ProjectListingComponents.ProjectAvatar src={`${process.env.REACT_APP_HOST}/${record.icon}`}
+                                                                alt={record.name}/>
                     </ProjectListingComponents.ProjectIcon>
                     {text}
                 </ProjectListingComponents.ProjectLink>
@@ -31,64 +33,51 @@ const ProjectListing = () => {
             dataIndex: 'lead',
             key: 'lead',
         }, {
-        title: 'Category',
-        key: 'category',
-        dataIndex: 'tag',
-        render: (_, { category }) => (
-            <Tag color='geekblue' key={category}>
-            {category.toUpperCase()}
-            </Tag>
-        ),
+            title: 'Category',
+            key: 'category',
+            dataIndex: 'tag',
+            render: (_, {category}) => (
+                <Tag color='geekblue' key={category}>
+                    {category.toUpperCase()}
+                </Tag>
+            ),
         }, {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
                     <a>Invite {record.name}</a>
                     <a>Delete</a>
-            </Space>
-        ),
-    },
-];
+                </Space>
+            ),
+        },
+    ];
 
     const [visible, setVisible] = useState(false);
     const [projects, setProjects] = useState([]);
-    const [searchText, setSearchText] = useState('');
     const [filteredData, setFilteredData] = useState([]);
+    const projectsData = useSelector((state) => state.DataSyncer.projectsData);
 
-    let authToken = localStorage.getItem('auth_token')
 
     useEffect(() => {
         const fetchProjects = async () => {
-            const response = await apiRequest
-                .get(`/api/projects/`, {
-                    headers: {
-                        Authorization: `Token ${authToken}`,
-                    },
-                })
-            .then(response => {
-                const dataArray = response.data.map(project => ({
-                    id: project.id,
-                    name: project.name,
-                    slug: project.key,
-                    lead: project.project_lead.username,
-                    icon: project.icon,
-                    category: typeof project.category === 'string' ? project.category : project.category.category
-                }));
-                setFilteredData(dataArray);
-                setProjects(dataArray);
-            })
-            .catch(error => {
-                displayErrorMessage(`Error occurred while fetching data: ${error}`);
-            });
-            setProjects(response.data);
+            const dataArray = projectsData.map(project => ({
+                id: project.id,
+                name: project.name,
+                slug: project.key,
+                lead: project.project_lead.username,
+                icon: project.icon,
+                category: typeof project.category === 'string' ? project.category : project.category.category
+            }));
+            setFilteredData(dataArray);
+            setProjects(dataArray);
         };
         fetchProjects()
     }, []);
 
     const handleSearch = (value) => {
         const filteredProjects = projects.filter(
-            (project) => project.name.toLowerCase().includes(value.toLowerCase())  ||
+            (project) => project.name.toLowerCase().includes(value.toLowerCase()) ||
                 project.slug.toLowerCase().includes(value.toLowerCase()) ||
                 project.lead.toLowerCase().includes(value.toLowerCase()) ||
                 project.category.toLowerCase().includes(value.toLowerCase())
@@ -128,14 +117,14 @@ const ProjectListing = () => {
                 <ProjectListingComponents.SearchInputContainer>
                     <ProjectListingComponents.SearchIcon/>
                     <ProjectListingComponents.SearchInput type="text" placeholder="Search Projects" value={null}
-                                    onChange={(e) => handleSearch(e.target.value)}/>
-                    </ProjectListingComponents.SearchInputContainer>
-                </ProjectListingComponents.SearchContainer>
+                                                          onChange={(e) => handleSearch(e.target.value)}/>
+                </ProjectListingComponents.SearchInputContainer>
+            </ProjectListingComponents.SearchContainer>
             <ProjectListingComponents.ProjectListingTable>
                 <Table
                     columns={columns}
                     dataSource={filteredData}
-                    pagination={{ defaultPageSize: 10, showSizeChanger: true }}
+                    pagination={{defaultPageSize: 10, showSizeChanger: true}}
                 />
             </ProjectListingComponents.ProjectListingTable>
         </>
