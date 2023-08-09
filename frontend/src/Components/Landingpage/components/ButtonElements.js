8aee4c86192f1161ebddc2c9f46eb36190660b16
Muhammad Asher <asher.khan@phpstudios.com>
2023-08-09 14:35:20 +0500

    Styles changes in SignUp button

--- frontend/src/Components/Landingpage/components/ButtonElements.js
+++ frontend/src/Components/Landingpage/components/ButtonElements.js
@@ -27,10 +27,10 @@ export const Button = styled(Link)`
 
 export const ButtonfromReactRouter = styled(RouterLink)`
   border-radius: 50px;
-  background: ${({primary}) => (primary ? '#1677FF' : '#010606')};
+  background: #1677FF;
   white-space: nowrap;
   padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
-  color: ${({dark}) => (dark ? '#010606' : '#fff')};
+  color: #000;
   font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
   outline: none;
   border: none;
@@ -43,7 +43,7 @@ export const ButtonfromReactRouter = styled(RouterLink)`
 
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: ${({primary}) => (primary ? '#000' : '#1677FF')};
+    background: #000;
     color: #fff;
   }
 `;
