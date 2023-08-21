431d98956d644bd97f4338af57879f37b607139f
Muhammad Asher <asher.khan@phpstudios.com>
2023-08-21 12:30:48 +0500

    Google Authentication

--- frontend/src/Store/store.js
+++ frontend/src/Store/store.js
@@ -3,12 +3,14 @@ import loginSlice from "./Slice/User/loginSlice"
 import signupSlice from "./Slice/User/signupSlice"
 import DataSyncerSlice from "./Slice/DataSyncerSlice"
 import IssueSlice from "./Slice/Issue/IssueSlice"
+import googleSlice from './Slice/User/googleauthSlice'
 
 export const store = configureStore({
     reducer: {
         login: loginSlice,
         signUp: signupSlice,
         DataSyncer: DataSyncerSlice,
-        issueData: IssueSlice
+        issueData: IssueSlice,
+        googleAuth: googleSlice,
     },
 })
