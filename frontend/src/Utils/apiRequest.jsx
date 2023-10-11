b25ab6232bbd3cbe336558a3e7b96b0db678dec7
Muhammad Asher <asher.khan@phpstudios.com>
2023-10-11 19:14:17 +0500

    Fix Api endpoint error

--- frontend/src/Utils/apiRequest.jsx
+++ frontend/src/Utils/apiRequest.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-    baseURL: `https://projex.nexius.ai`,
+    baseURL: `https://projex.nexius.ai/api`,
     withCredentials: true,
 });
 
